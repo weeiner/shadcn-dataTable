64091372aaded9dbfba092ae465487b0845ce485
weeiner <71634275+weeiner@users.noreply.github.com>
2024-10-20 18:51:03 +0800

    feat: changed to a single .jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import { DataTable } from "./payments/data-table";
 import { columns } from "./payments/columns"; // Import columns
 import { payments } from "./payments/payments"; // Import payments or define your data here
+import { NewDataTable } from "./components/NewDataTable";
 
 function App() {
   // Example data and columns for the DataTable component
   return (
     <div>
-      <DataTable columns={columns} data={payments} />
-      <div className="flex justify-around border border-black">
-        <div>test</div>
-        <div>test</div>
-      </div>
+      {/* <DataTable columns={columns} data={payments} /> */}
+      <NewDataTable />
     </div>
   );
 }
